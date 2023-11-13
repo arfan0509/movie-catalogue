1c3bae266ff641a55fa73182ffa5ff2afb65d3ec
arfan0509 <arfanraja89@gmail.com>
2023-11-13 17:27:56 +0700

    Add dotenv and reconfigure webpack and jest

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: '97a8dfe4b95fd24140ca50bcf586961e',
+  KEY: process.env.API_KEY_TMDB,
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
